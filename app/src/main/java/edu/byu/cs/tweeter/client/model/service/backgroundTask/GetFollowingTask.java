0aa8d947ea14f39bf5433e68bbea8eb6304c6608
logankimbs <logankimbs@gmail.com>
2023-02-15 20:04:50 -0700

    added following

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/GetFollowingTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/GetFollowingTask.java
@@ -1,14 +1,11 @@
 package edu.byu.cs.tweeter.client.model.service.backgroundTask;
 
-import android.os.Bundle;
 import android.os.Handler;
 
-import java.io.Serializable;
 import java.util.List;
 
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
-import edu.byu.cs.tweeter.util.FakeData;
 import edu.byu.cs.tweeter.util.Pair;
 
 /**
