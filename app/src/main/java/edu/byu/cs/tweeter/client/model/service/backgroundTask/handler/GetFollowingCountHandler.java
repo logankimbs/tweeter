5967613b3195a6c283d0ca884236d2ee666e994d
logankimbs <logankimbs@gmail.com>
2023-02-17 12:44:09 -0700

    more count

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/handler/GetFollowingCountHandler.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/handler/GetFollowingCountHandler.java
@@ -1,35 +1,11 @@
 package edu.byu.cs.tweeter.client.model.service.backgroundTask.handler;
 
-import android.os.Handler;
 import android.os.Looper;
-import android.os.Message;
-
-import androidx.annotation.NonNull;
 
 import edu.byu.cs.tweeter.client.model.service.MainService;
-import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFollowingCountTask;
 
-public class GetFollowingCountHandler extends Handler {
-    private MainService.GetFollowingCountObserver observer;
-
+public class GetFollowingCountHandler extends CountHandler {
     public GetFollowingCountHandler(MainService.GetFollowingCountObserver observer) {
-        super(Looper.getMainLooper());
-        this.observer = observer;
-    }
-
-    @Override
-    public void handleMessage(@NonNull Message msg) {
-        boolean success = msg.getData().getBoolean(GetFollowingCountTask.SUCCESS_KEY);
-
-        if (success) {
-            int count = msg.getData().getInt(GetFollowingCountTask.COUNT_KEY);
-            observer.handleSuccess(count);
-        } else if (msg.getData().containsKey(GetFollowingCountTask.MESSAGE_KEY)) {
-            String message = msg.getData().getString(GetFollowingCountTask.MESSAGE_KEY);
-            observer.handleFailure(message);
-        } else if (msg.getData().containsKey(GetFollowingCountTask.EXCEPTION_KEY)) {
-            Exception ex = (Exception) msg.getData().getSerializable(GetFollowingCountTask.EXCEPTION_KEY);
-            observer.handleException(ex);
-        }
+        super(Looper.getMainLooper(), observer);
     }
 }
