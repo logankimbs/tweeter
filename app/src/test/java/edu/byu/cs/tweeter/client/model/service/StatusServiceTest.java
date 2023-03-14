111d473cd737c44de29131fcb8a5692232eae5c3
logankimbs <logankimbs@gmail.com>
2023-03-14 17:40:16 -0600

    made StatusServiceTest

---
+++ app/src/test/java/edu/byu/cs/tweeter/client/model/service/StatusServiceTest.java
@@ -0,0 +1,12 @@
+package edu.byu.cs.tweeter.client.model.service;
+
+public class StatusServiceTest {
+    // Using JUnit and Mockito, write an INTEGRATION test for your client-side Service that
+    // returns a user's story pages (i.e., StatusService). The Service should have an operation
+    // that creates a background task to retrieve a user's story from the server and notifies the
+    // Service's observer of the operation's outcome. Your test should verify that the Service's
+    // observer is notified in the case of a successful story retrieval.
+    // Note: you do NOT have to write tests for the other outcomes (i.e., failing not because of
+    // an exception, failing because of an exception).
+
+}
