44e1bb7a3b638c14c0b3443a4bb423a5e6b4b312
logankimbs <logankimbs@gmail.com>
2023-03-14 14:10:33 -0600

    added registerhandler and added register method to iserservice

--- server/src/main/java/edu/byu/cs/tweeter/server/service/UserService.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/service/UserService.java
@@ -3,7 +3,9 @@ package edu.byu.cs.tweeter.server.service;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
 import edu.byu.cs.tweeter.model.net.request.LoginRequest;
+import edu.byu.cs.tweeter.model.net.request.RegisterRequest;
 import edu.byu.cs.tweeter.model.net.response.LoginResponse;
+import edu.byu.cs.tweeter.model.net.response.RegisterResponse;
 import edu.byu.cs.tweeter.util.FakeData;
 
 public class UserService {
@@ -19,6 +21,19 @@ public class UserService {
         User user = getDummyUser();
         AuthToken authToken = getDummyAuthToken();
         return new LoginResponse(user, authToken);
+    }
+
+    public RegisterResponse register(RegisterRequest request) {
+        if (request.getAlias() == null) {
+            throw new RuntimeException("[Bad Request] Missing a username");
+        } else if (request.getPassword() == null) {
+            throw new RuntimeException("[Bad Request] Missing a password");
+        }
+
+        // TODO: Generates dummy data. Replace with a real implementation.
+        User user = getDummyUser();
+        AuthToken authToken = getDummyAuthToken();
+        return new RegisterResponse(user, authToken);
     }
 
     /**
