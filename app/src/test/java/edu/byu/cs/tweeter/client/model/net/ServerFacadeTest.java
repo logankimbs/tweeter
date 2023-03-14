2a34a9d2ec69ee3fbafa32936ec0b57b76a61f6c
logankimbs <logankimbs@gmail.com>
2023-03-14 17:34:32 -0600

    outlined server facade test

---
+++ app/src/test/java/edu/byu/cs/tweeter/client/model/net/ServerFacadeTest.java
@@ -0,0 +1,34 @@
+package edu.byu.cs.tweeter.client.model.net;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ServerFacadeTest {
+    // Write automated integration tests to verify that your client-side Server Facade class
+    // correctly communicates with your server. The test code can directly call the ServerFacade class
+    // (i.e., no need to involve Services or Presenters). Test the following features:
+    //  - Register
+    //  - GetFollowers
+    //  - GetFollowingCount
+
+    @BeforeEach
+    public void setup() {}
+
+    @Test
+    public void testRegister_successful() {}
+
+    @Test
+    public void testRegister_failed() {}
+
+    @Test
+    public void testGetFollowers_successful() {}
+
+    @Test
+    public void testGetFollowers_failed() {}
+
+    @Test
+    public void testGetFollowingCount_successful() {}
+
+    @Test
+    public void testGetFollowingCount_failed() {}
+}
