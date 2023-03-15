35ee22f459b901ab7efd19e4923f82202fa1140a
logankimbs <logankimbs@gmail.com>
2023-03-15 15:46:32 -0600

    added to followeecount

--- shared/src/main/java/edu/byu/cs/tweeter/model/net/request/FolloweeCountRequest.java
+++ shared/src/main/java/edu/byu/cs/tweeter/model/net/request/FolloweeCountRequest.java
@@ -1,4 +1,32 @@
 package edu.byu.cs.tweeter.model.net.request;
 
 public class FolloweeCountRequest {
+    private String alias;
+
+    /**
+     * Allows construction of the object from Json. Private so it won't be called in normal code.
+     */
+    private FolloweeCountRequest() {}
+
+    public FolloweeCountRequest(String alias) {
+        this.alias = alias;
+    }
+
+    /**
+     * Returns the followee whose followers are to be returned by this request.
+     *
+     * @return the followee.
+     */
+    public String getAlias() {
+        return alias;
+    }
+
+    /**
+     * Sets the followee.
+     *
+     * @param alias the followee.
+     */
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
 }
--- shared/src/main/java/edu/byu/cs/tweeter/model/net/response/FolloweeCountResponse.java
+++ shared/src/main/java/edu/byu/cs/tweeter/model/net/response/FolloweeCountResponse.java
@@ -1,4 +1,20 @@
 package edu.byu.cs.tweeter.model.net.response;
 
-public class FolloweeCountResponse {
+public class FolloweeCountResponse extends Response {
+    private int count;
+
+    // 400 Bad Request
+    private FolloweeCountResponse(String message) {
+        super(false, message);
+    }
+
+    // 200 OK
+    public FolloweeCountResponse(int count, boolean success) {
+        super(success);
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
