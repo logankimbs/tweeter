edb70e6db2ced80fa4d6493d6b5c6840adad0aef
logankimbs <logankimbs@gmail.com>
2023-03-18 11:06:02 -0600

    getuser handler

--- server/src/main/java/edu/byu/cs/tweeter/server/service/UserService.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/service/UserService.java
@@ -2,10 +2,13 @@ package edu.byu.cs.tweeter.server.service;
 
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
+import edu.byu.cs.tweeter.model.net.request.GetUserRequest;
 import edu.byu.cs.tweeter.model.net.request.LoginRequest;
 import edu.byu.cs.tweeter.model.net.request.RegisterRequest;
+import edu.byu.cs.tweeter.model.net.response.GetUserResponse;
 import edu.byu.cs.tweeter.model.net.response.LoginResponse;
 import edu.byu.cs.tweeter.model.net.response.RegisterResponse;
+import edu.byu.cs.tweeter.server.dao.UserDAO;
 import edu.byu.cs.tweeter.util.FakeData;
 
 public class UserService {
@@ -70,5 +73,15 @@ public class UserService {
      */
     FakeData getFakeData() {
         return FakeData.getInstance();
+    }
+
+    public GetUserResponse getUser(GetUserRequest request) {
+        if(request.getAlias() == null){
+            throw new RuntimeException("[Bad Request] Missing a userAlias");
+        }
+
+        // TODO: Generates dummy data. Replace with a real implementation.
+        User user = getFakeData().findUserByAlias(request.getAlias());
+        return new GetUserResponse(user);
     }
 }
